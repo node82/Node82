ff56643ede2ffc9571a39c0418d697e843310c87
Rick Plyler <rick@node82.com>
2019-09-27 15:56:58 -0400

    Create imgConv.py

---
+++ imgConv.py
@@ -0,0 +1,21 @@
+#converts .jfif images to .jpg 
+import os
+from PIL import Image
+
+yourpath = ('/insert location here')
+for root, dirs, files in os.walk(yourpath, topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+        if os.path.splitext(os.path.join(root, name))[1].lower() == ".jfif":
+            if os.path.isfile(os.path.splitext(os.path.join(root, name))[0] + ".jpg"):
+                print("A jpeg file already exists for %s" % name)
+            # If a jpeg is *NOT* present, create one from the tiff.
+            else:
+                outfile = os.path.splitext(os.path.join(root, name))[0] + ".jpg"
+                try:
+                    im = Image.open(os.path.join(root, name))
+                    print("Generating jpeg for %s" % name)
+                    im.thumbnail(im.size)
+                    im.save(outfile, "JPEG", quality=100)
+                except:
+                    print("whoops")
