928e8cf934f566a70b35ac641ef50893d294d024
Rick Plyler <rick@node82.com>
2020-07-11 18:49:00 -0400

    Create stringReplace.php

---
+++ php/stringReplace.php
@@ -0,0 +1,5 @@
+<?php
+$variable = "This is the true story, of seven strangers, picked to live in a house, work together, and have their lives taped. Find out what happens, when people stop being polite, and start getting real… The Real World!"
+$new_variable = str_replace("seven", "one", $variable);
+echo $new_variable;
+?>
