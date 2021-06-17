41f51870ddd709fc04863d968cc505308d070a4b
Rick Plyler <rick@node82.com>
2021-06-16 21:23:56 -0400

    Create components.py

---
+++ python/components.py
@@ -0,0 +1,10 @@
+#####################
+import time
+startTime = time.time()
+
+executionTime = (time.time() - startTime)
+print('Execution time in sec.: ' + str(executionTime))
+
+#####################
+
+#####################
