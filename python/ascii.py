4e2c07a34d66c507eef0593b3d5fb518849e2644
Rick Plyler <rick@node82.com>
2021-08-03 14:54:41 -0400

    Create ascii.py

---
+++ python/ascii.py
@@ -0,0 +1,13 @@
+strTable = "<html><table><tr><th>Char</th><th>ASCII</th></tr>"
+ 
+for num in range(33,48):
+ symb = chr(num)
+ strRW = "<tr><td>"+str(symb)+ "</td><td>"+str(num)+"</td></tr>"
+ strTable = strTable+strRW
+ 
+strTable = strTable+"</table></html>"
+ 
+hs = open("asciiCharHTMLTable.html", 'w')
+hs.write(strTable)
+ 
+print strTable
