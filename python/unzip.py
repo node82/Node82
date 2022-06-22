9587cf34308d36456b8bdc9b5a672a7c022b818a
Rick Plyler <rick@node82.com>
2022-06-21 20:31:20 -0400

    Update unzip.py

--- python/unzip.py
+++ python/unzip.py
@@ -1,8 +1,8 @@
 def unzip():
-import zipfile
-filePath = '$PATH'
-extractPath = '$EPATH'
-with zipfile.ZipFile(filePath, 'r') as zip_ref:
-    zip_ref.extractall(extractPath)
+    import zipfile
+    filePath = '$PATH'
+    extractPath = '$EPATH'
+    with zipfile.ZipFile(filePath, 'r') as zip_ref:
+        zip_ref.extractall(extractPath)
     
 unzip()
